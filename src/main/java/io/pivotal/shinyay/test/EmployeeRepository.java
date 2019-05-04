f1a07b52c3b50752e96dfce165e2add06fabd41b
shinyay <shinya.com@gmail.com>
2019-05-04 17:14:24 +0900

    Add Repository class for Employee model

---
+++ src/main/java/io/pivotal/shinyay/test/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package io.pivotal.shinyay.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Repository
+@Transactional
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    public Employee findByName(String name);
+
+    public List<Employee> findAll();
+
+}
