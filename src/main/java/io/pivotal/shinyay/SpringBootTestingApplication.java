a7d48c7d21254c2ddfa1ffea716382446cd73aa5
shinyay <shinya.com@gmail.com>
2019-05-04 17:08:12 +0900

    Modify package

--- src/main/java/io/pivotal/shinyay/SpringBootTestingApplication.java
+++ src/main/java/io/pivotal/shinyay/SpringBootTestingApplication.java
@@ -1,4 +1,4 @@
-package io.pivotal.shinyay.test;
+package io.pivotal.shinyay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
