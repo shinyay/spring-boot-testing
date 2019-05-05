3d1218527f96542477e37c52cf0044ba29b43880
shinyay <shinya.com@gmail.com>
2019-05-05 10:30:07 +0900

    Add persistence layer test

---
+++ src/test/java/io/pivotal/shinyay/test/EmployeeRepositoryIntegrationTest.java
@@ -0,0 +1,30 @@
+package io.pivotal.shinyay.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EmployeeRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void whenFindByName_thenReturnEmployee() {
+
+        Employee emp = new Employee("Shinya");
+        entityManager.persistAndFlush(emp);
+
+        Employee result= employeeRepository.findByName(emp.getName());
+        assertThat(result.getName()).isEqualTo(emp.getName());
+    }
+}
