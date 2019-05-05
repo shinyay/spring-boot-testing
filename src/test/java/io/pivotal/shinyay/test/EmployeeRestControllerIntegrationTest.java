2c7a445c7dfdc8c0f45d43f4ad41f6b61a979650
shinyay <shinya.com@gmail.com>
2019-05-05 15:52:44 +0900

    Add Controller Integration Test

---
+++ src/test/java/io/pivotal/shinyay/test/EmployeeRestControllerIntegrationTest.java
@@ -0,0 +1,56 @@
+package io.pivotal.shinyay.test;
+
+import io.pivotal.shinyay.SpringBootTestingApplication;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = SpringBootTestingApplication.class)
+@AutoConfigureMockMvc
+public class EmployeeRestControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @After
+    public void resetDb() {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void givenEmployees_whenGetEmployees_thenStatusOK() throws Exception {
+        createTestEmployee("Name1");
+        createTestEmployee("Name2");
+
+        mvc.perform(get("/api/employees").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(2))))
+                .andExpect(jsonPath("$[0].name", is("Name1")))
+                .andExpect(jsonPath("$[1].name", is("Name2")));
+    }
+
+    private void createTestEmployee(String name) {
+        Employee emp = new Employee(name);
+        repository.saveAndFlush(emp);
+    }
+}
